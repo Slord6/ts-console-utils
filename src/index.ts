b22f813745b3c79afae768e54e7518160024090f
Sam <github@samlord.co.uk>
2026-03-28 21:12:26 +0000

    Move index back, update package

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * as Characters from "./characters";
-export * as Colour from "./colour";
-export * as Control from "./control";
-export * as Progress from "./progress";
-export * as Text from "./text";
