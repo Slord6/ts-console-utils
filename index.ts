0c5e7d26baa22e780096cfecb06b053b900439d8
Sam <github@samlord.co.uk>
2026-03-28 21:10:31 +0000

    Move index

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * as Characters from "./src/characters";
-export * as Colour from "./src/colour";
-export * as Control from "./src/control";
-export * as Progress from "./src/progress";
-export * as Text from "./src/text";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as Characters from "./characters";
+export * as Colour from "./colour";
+export * as Control from "./control";
+export * as Progress from "./progress";
+export * as Text from "./text";
